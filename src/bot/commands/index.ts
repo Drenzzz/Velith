c1929cf88453ed622acb20ae03b539c69de0acf8
Drenzzz <realdrenzzz@gmail.com>
2026-08-17 11:41:39 +0700

    chore(commands): register all M5 commands

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,6 +1,11 @@
 import { Collection, type ChatInputCommandInteraction } from "discord.js";
 import { waifuCommand } from "./waifu.ts";
 import { setupCommand } from "./setup.ts";
+import { haremCommand } from "./harem.ts";
+import { profileCommand } from "./profile.ts";
+import { leaderboardCommand } from "./leaderboard.ts";
+import { historyCommand } from "./history.ts";
+import { adminRerollCommand } from "./admin.ts";
 
 export interface CommandModule {
   data: { name: string; toJSON: () => unknown };
@@ -10,5 +15,18 @@ export interface CommandModule {
 export const commands = new Collection<string, CommandModule>();
 commands.set(waifuCommand.data.name, waifuCommand);
 commands.set(setupCommand.data.name, setupCommand);
+commands.set(haremCommand.data.name, haremCommand);
+commands.set(profileCommand.data.name, profileCommand);
+commands.set(leaderboardCommand.data.name, leaderboardCommand);
+commands.set(historyCommand.data.name, historyCommand);
+commands.set(adminRerollCommand.data.name, adminRerollCommand);
 
-export const commandData = [waifuCommand.data.toJSON(), setupCommand.data.toJSON()];
+export const commandData = [
+  waifuCommand.data.toJSON(),
+  setupCommand.data.toJSON(),
+  haremCommand.data.toJSON(),
+  profileCommand.data.toJSON(),
+  leaderboardCommand.data.toJSON(),
+  historyCommand.data.toJSON(),
+  adminRerollCommand.data.toJSON(),
+];
